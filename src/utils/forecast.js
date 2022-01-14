bf88c2f4ca2501ba2e5ee6211bbb47eb03eb9b7f
ashraf khaled <ashraf.badr20@gmail.com>
2022-01-14 17:11:47 +0200

    displaying more weather info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,6 +2,7 @@ const request = require('request')
 
 const forecast = (la , lo, callback ) => {
     const url = 'http://api.weatherstack.com/current?access_key=2553852097137e14d54f826ef5269e71&query='+la+','+lo+''
+    console.log(url)
     request( {
         url,
         json: true,
@@ -11,8 +12,8 @@ const forecast = (la , lo, callback ) => {
         } else if (body.error) {
             callback('unable to find the location', undefined)
         } else {
-            const {weather_descriptions, temperature, feelslike} = body.current
-            callback(undefined, weather_descriptions[0] + ' and the current temperature '+ temperature+' C but it feels like '+ feelslike+ ' C')
+            const {weather_descriptions, temperature, feelslike, humidity} = body.current
+            callback(undefined, weather_descriptions[0] + ' and the current temperature '+ temperature+' C but it feels like '+ feelslike+ ' C and the Humidity is '+ humidity +'%')
         }
     })
 }
