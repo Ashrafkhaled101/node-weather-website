b4650d97ce29dfbf83f15626c700221839bfbeac
ashraf khaled <ashraf.badr20@gmail.com>
2022-01-14 17:18:20 +0200

    removing console.log

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const forecast = (la , lo, callback ) => {
     const url = 'http://api.weatherstack.com/current?access_key=2553852097137e14d54f826ef5269e71&query='+la+','+lo+''
-    console.log(url)
     request( {
         url,
         json: true,
@@ -13,7 +12,7 @@ const forecast = (la , lo, callback ) => {
             callback('unable to find the location', undefined)
         } else {
             const {weather_descriptions, temperature, feelslike, humidity} = body.current
-            callback(undefined, weather_descriptions[0] + ' and the current temperature '+ temperature+' C but it feels like '+ feelslike+ ' C and the Humidity is '+ humidity +'%')
+            callback(undefined, weather_descriptions[0] + ' and the current temperature '+ temperature+' C but it feels like '+ feelslike+ ' C ,and the Humidity is '+ humidity +'%')
         }
     })
 }
